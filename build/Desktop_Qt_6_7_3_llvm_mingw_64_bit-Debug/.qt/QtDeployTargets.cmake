76ff06c59f7042387c3b8bc75c77a4e0d08bbe90
@sgly123 <2388594519@qq.com>
2025-05-14 10:28:26 +0800

    README

--- build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_IM_Client_FILE D:/IM_Client/IM_Client/build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/IM_Client.exe)
-set(__QT_DEPLOY_TARGET_IM_Client_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_IM_Client_RUNTIME_DLLS D:/qt/6.7.3/llvm-mingw_64/bin/Qt6Network.dll;D:/qt/6.7.3/llvm-mingw_64/bin/Qt6Widgets.dll;D:/qt/6.7.3/llvm-mingw_64/bin/Qt6Gui.dll;D:/qt/6.7.3/llvm-mingw_64/bin/Qt6Core.dll)
